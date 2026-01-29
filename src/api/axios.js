78aff126c25507d2c17aa6e8968f191e9c6c7e0c
blzgokul <blackzzayn@gmail.com>
2026-01-29 16:50:45 +0530

    Fix axios baseURL syntax error

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,26 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: ""https://your-backend-url/api""
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
-
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
-  }
-  return req;
-});
-
-API.interceptors.response.use(
-  (res) => res,
-  (err) => {
-    if (err.response?.status === 401) {
-      localStorage.removeItem("token");
-      window.location.href = "/login";
-    }
-    return Promise.reject(err);
-  }
-);
 
 export default API;
